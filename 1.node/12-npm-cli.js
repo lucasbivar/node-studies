4ea52b8b83ac1b12291ba2dc24a2b744ac7ddccb
lucasbivar <lucasbivarfonseca@gmail.com>
2021-05-13 20:23:36 -0300

    adding async pattern with blocking code

--- 1.node/12-npm-cli.js
+++ 1.node/12-npm-cli.js
@@ -8,10 +8,19 @@
 // npm install -g <packageName>
 // sudo npm install -g <packageName> (mac)
 
+//install with dev dependecy
+// npm i nodemon -D
+
 // package.json - manifest file (stores important info about project/package)
 // manual approach (create package.json in the root, create properties etc)
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
+
+// npm install - install all dependencies
+
+// npm uninstall packageName - uninstall specific package
+
+//https://nodesource.com/blog/the-basics-of-package-json-in-node-js-and-npm/
 
 const _ = require("lodash");
 
