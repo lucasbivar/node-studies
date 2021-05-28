5f3d9c6099f7c235586ea5e4798b842e7161f742
lucasbivar <lucasbivarfonseca@gmail.com>
2021-05-28 14:25:48 -0300

    adding some notes

--- 4.react-basics/src/Book.js
+++ 4.react-basics/src/Book.js
@@ -1,34 +1,42 @@
-import React from 'react'
+import React from "react";
+
+// JSX Rules
+// return single element
+// div / section / article or Fragment (React Fragment <> </><- empty tag)
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formating
 
 const Book = ({ img, title, author }) => {
- // attribute, eventHandler
- // onClick, onMouseOver
- const clickHandler = (e) => {
-  console.log(e);
-  console.log(e.target);
-  alert('hello world');
- };
- const complexExample = (author) => {
-  console.log(author);
- };
- return (
-  <article
-   className='book'
-   onMouseOver={() => {
-    console.log(title);
-   }}
-  >
-   <img src={img} alt='' />
-   <h1 onClick={() => console.log(title)}>{title}</h1>
-   <h4>{author}</h4>
-   <button type='button' onClick={clickHandler}>
-    reference example
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello world");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
       </button>
-   <button type='button' onClick={() => complexExample(author)}>
-    more complex example
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
       </button>
-  </article>
- );
+    </article>
+  );
 };
 
-export default Book
+export default Book;
