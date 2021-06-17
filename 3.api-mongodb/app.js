49279537cdfef7355d0d6007b3578ca94c0cca24
lucasbivar <lucasbivarfonseca@gmail.com>
2021-06-17 18:24:45 -0300

    setup swagger auto docs

--- 3.api-mongodb/app.js
+++ 3.api-mongodb/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const player = require('./routes/PlayerRoutes')
 const team = require('./routes/TeamRoutes')
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -13,10 +17,40 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+	swaggerDefinition: {
+		info : {
+			version: "1.0.0",
+			title: "Soccer Team API",
+			description: 'Simple Soccer Team API Information',
+			"license": {
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT"
+      },
+			contact: {
+				name: "Lucas Bivar",
+				url: "https://github.com/lucasbivar",
+				email: "lucasbivarfonseca@hotmail.com"
+			},
+			servers: [ {
+				"url": "http://localhost:5000",
+				"description": "Development server"
+			}]
+		},
+		"host": "localhost:5000",
+    "basePath": "/api",
+	},
+	apis: ['./routes/*.js']
+}
+
+
 // set routes
 app.use('/api', player) 
 app.use('/api', team)
 
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, ()=>{
 	console.log('Server is listening on port 5000....');
