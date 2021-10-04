df2564dabc0185919decce0e8002b2c72c6d0669
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-10-04 19:15:55 -0300

    adding js

---
+++ 8. assuntos-js/objects.js
@@ -0,0 +1,17 @@
+const pessoas = {
+  nome: "Vini",
+  sobrenome: "Miranda",
+  idade: 30,
+  endereco: {
+    rua: "Av Brasil",
+    numero: 320
+  }
+};
+
+const { nome } = pessoas;
+
+console.log(nome);
+
+const { idade } = pessoas;
+
+console.log(idade);
